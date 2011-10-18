4584a747399dcfa9c4cfe5489f814dab9a9b65b6
Peter Petersson <petersson.peter@gmail.com>
2011-10-18 17:17:15 +0200

    Writing some comments and adding some simple style to infoWindow data to show that it accepts html/css styling.

--- src/main/scala/code/snippet/GoogleMaps.scala
+++ src/main/scala/code/snippet/GoogleMaps.scala
@@ -19,7 +19,7 @@ class GoogleMaps {
   // replace the contents of the element with what map to render
   def jsData = renderGoogleMap()
 
-  // converts a the location into a JSON Object
+  // converts a the location into a json object
   def makeLocation(title: String, lat: String, lng: String,iwc: String): JsObj = {
     JsObj(("title", title),
       ("lat", lat),
@@ -30,13 +30,16 @@ class GoogleMaps {
    // called by renderGoogleMap which passes the list of locations 
    // into the javascript function as json objects
   def ajaxFunc(locobj: List[JsObj]): JsCmd = {
+    //JsCrVar is a JSCmds type that creates the java script var named "locations" and assingns it to
+    //the JsObj using the js.JE type member JsArray to hold the objects sj.JE type member JsRaw holds 
+    //the expression that will be evaluated 
     JsCrVar("locations", JsObj(("loc", JsArray(locobj: _*)))) & JsRaw("drawmap(locations)").cmd
   }
 
   // render the google map
   def renderGoogleMap(): NodeSeq = {
     // setup some locations (Marker/infoWindow content) to display on the map
-	val locations: List[JsObj] = List(makeLocation("loc1","40.744715", "-74.0046","Info window content text for loc1 ....."),makeLocation("loc2","40.75684", "-73.9966","Info window content text for loc2 ....."))
+	val locations: List[JsObj] = List(makeLocation("loc1","40.744715", "-74.0046","Info window content text for loc1 ....."),makeLocation("loc2","40.75684", "-73.9966","<div style='border: 1px solid #000;margin:5px;color:green'>Info window content text for loc2 .....</div>"))
 	
 	// where the magic happens
     (<head_merge>
